193b1e131ebc92ec6138c6ef5aaa8bdec27045ef
EllisCallaghan <callaghanellis440@gmail.com>
2023-05-22 09:24:11 +0800

    first commit

--- app/components/MovieCard.jsx
+++ app/components/MovieCard.jsx
@@ -42,7 +42,7 @@ const MovieCard = ({title,image,showNum,index,movieId,desc}) => {
     for(let i =index; i<showNum;i++){
 
       return (
-        <Link onTouchStart={(e) => handleHover(e)} 
+        <Link className='touch-handler'
         
         onMouseEnter={(e) => handleHover(e)} onMouseLeave={() => setHover(false)} 
         passHref href={`/movie/${encodeURIComponent(movieId)}`} >
